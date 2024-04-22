dc888733ecbde3cd221fcca90aa4fa86c2b95e2e
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:42:30 -0300

    Update punto12.js

--- punto12.js
+++ punto12.js
@@ -2,7 +2,7 @@ function calcularCircunferenciaCirculo(radio) {
     return 2 * Math.PI * radio;
 }
 
-// Ejemplo de uso:
+
 let radio = 3;
 let circunferencia = calcularCircunferenciaCirculo(radio);
 console.log("La circunferencia del círculo con radio", radio, "es:", circunferencia.toFixed(2));
