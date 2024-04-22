f18034ff430fa60c6055b7081015545c35e7b319
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:47:49 -0300

    Update punto24.js

--- punto24.js
+++ punto24.js
@@ -1,8 +1,8 @@
 function imprimirValoresRepetidos(array) {
-    // Objeto para realizar un seguimiento de cuántas veces aparece cada valor
+  
     let conteo = {};
 
-    // Iterar sobre la array y contar cuántas veces aparece cada valor
+  
     for (let i = 0; i < array.length; i++) {
         let valor = array[i];
         if (conteo[valor]) {
@@ -12,7 +12,6 @@ function imprimirValoresRepetidos(array) {
         }
     }
 
-    // Iterar sobre el objeto de conteo y imprimir los valores que se repiten
     let valoresRepetidos = [];
     for (let clave in conteo) {
         if (conteo[clave] > 1) {
@@ -20,10 +19,8 @@ function imprimirValoresRepetidos(array) {
         }
     }
 
-    // Imprimir los valores repetidos
     console.log("Valores que se repiten:", valoresRepetidos.join(", "));
 }
 
-// Ejemplo de uso:
 let array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
-imprimirValoresRepetidos(array); // Output: Valores que se repiten: 6, 23, 33, 100
+imprimirValoresRepetidos(array); 
