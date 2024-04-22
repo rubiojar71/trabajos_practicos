e03c478ac9845137bba9b2894f3b75194a5b2ca4
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:42:43 -0300

    Update punto13.js

--- punto13.js
+++ punto13.js
@@ -2,7 +2,7 @@ function calcularAreaCirculo(radio) {
     return Math.PI * radio * radio;
 }
 
-// Ejemplo de uso:
+
 let radio = 4;
 let area = calcularAreaCirculo(radio);
 console.log("El área del círculo con radio", radio, "es:", area.toFixed(2));
