e3543ff92739638c09c428af675687662e925d94
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:41:21 -0300

    Update punto10.js

--- punto10.js
+++ punto10.js
@@ -2,7 +2,6 @@ function calcularPerimetroCuadrado(longitudLado) {
     return longitudLado * 4;
 }
 
-// Ejemplo de uso:
 let longitud = 5;
 let perimetro = calcularPerimetroCuadrado(longitud);
 console.log("El perímetro del cuadrado con lado", longitud, "es:", perimetro);
