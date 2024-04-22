631443628e81a31689a5d8502e46149fb454d314
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:43:05 -0300

    Update punto14.js

--- punto14.js
+++ punto14.js
@@ -1,16 +1,16 @@
 function cantidadDiasMes(numeroMes) {
-    // Array con la cantidad de días por mes (suponiendo que no es un año bisiesto)
+    
     const diasPorMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    // Verificar si el número de mes está dentro del rango válido (1 a 12)
+   
     if (numeroMes >= 1 && numeroMes <= 12) {
-        return diasPorMes[numeroMes - 1]; // Restamos 1 porque los índices del array comienzan desde 0
+        return diasPorMes[numeroMes - 1]; 
     } else {
         return "Número de mes inválido";
     }
 }
 
-// Ejemplo de uso:
-let mes = 3; // Por ejemplo, marzo
+
+let mes = 3; 
 let cantidadDias = cantidadDiasMes(mes);
 console.log("El mes", mes, "tiene", cantidadDias, "días.");
