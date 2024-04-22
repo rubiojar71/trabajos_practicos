32038cb6f7e045eb35a79bdb2949ee8419bc9a8e
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:49:36 -0300

    Update punto6.js

--- punto6.js
+++ punto6.js
@@ -2,7 +2,7 @@ function cuadrado(numero) {
     return numero * numero;
 }
 
-// Ejemplo de uso:
+
 let numero = 4;
 let resultado = cuadrado(numero);
 console.log("El cuadrado de", numero, "es:", resultado); 
