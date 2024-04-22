5e43331c77582aa318e3d96462fda72e508a51c7
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:47:01 -0300

    Update punto22.js

--- punto22.js
+++ punto22.js
@@ -4,9 +4,9 @@ function imprimirNumeroMasBajo(array) {
         return;
     }
 
-    let minimo = array[0]; // Suponemos que el primer elemento es el mínimo
+    let minimo = array[0];
 
-    // Iteramos sobre el array para encontrar el número más bajo
+   
     for (let i = 1; i < array.length; i++) {
         if (array[i] < minimo) {
             minimo = array[i];
@@ -16,6 +16,5 @@ function imprimirNumeroMasBajo(array) {
     console.log("El número más bajo del array es:", minimo);
 }
 
-// Ejemplo de uso:
 let numeros = [5, 3, 9, 1, 7];
-imprimirNumeroMasBajo(numeros); // Output: El número más bajo del array es: 1
+imprimirNumeroMasBajo(numeros); 
