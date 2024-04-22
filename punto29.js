a52465007fac2d23724115b325a43cf7a21422d6
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:49:00 -0300

    Update punto29.js

--- punto29.js
+++ punto29.js
@@ -1,11 +1,10 @@
 function palabraMasLarga(frase) {
-    // Dividir la frase en palabras
+
     let palabras = frase.split(' ');
 
-    // Inicializar una variable para almacenar la palabra más larga
+
     let palabraMasLarga = '';
 
-    // Iterar sobre cada palabra y comparar su longitud con la longitud de la palabra más larga
     for (let i = 0; i < palabras.length; i++) {
         if (palabras[i].length > palabraMasLarga.length) {
             palabraMasLarga = palabras[i];
@@ -15,7 +14,6 @@ function palabraMasLarga(frase) {
     return palabraMasLarga;
 }
 
-// Ejemplo de uso:
 let x = "Tutorial de desarrollo web";
 let resultado = palabraMasLarga(x);
-console.log(resultado); // Output: "desarrollo"
+console.log(resultado); 
