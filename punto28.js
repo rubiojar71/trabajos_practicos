dc23079528c42b22eb7fb62acd0bd19f9eb00800
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:48:42 -0300

    Update punto28.js

--- punto28.js
+++ punto28.js
@@ -1,19 +1,19 @@
 function capitalizarPrimerasLetras(cadena) {
-    // Dividir la cadena en palabras
+
     let palabras = cadena.split(' ');
 
-    // Iterar sobre cada palabra y capitalizar la primera letra
+
     for (let i = 0; i < palabras.length; i++) {
         palabras[i] = palabras[i].charAt(0).toUpperCase() + palabras[i].slice(1);
     }
 
-    // Unir las palabras en una cadena
+ 
     let cadenaCapitalizada = palabras.join(' ');
 
     return cadenaCapitalizada;
 }
 
-// Ejemplo de uso:
+
 let x = "prince of persia";
 let resultado = capitalizarPrimerasLetras(x);
-console.log(resultado); // Output: "Prince Of Persia"
+console.log(resultado); 
