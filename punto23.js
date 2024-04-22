3e5e1cc9e646e1d87669a80a70d3d02985f81c71
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:47:19 -0300

    Update punto23.js

--- punto23.js
+++ punto23.js
@@ -1,5 +1,5 @@
 function imprimirValorEnIndice(array, indice) {
-    // Verificar si el índice está dentro del rango válido
+   
     if (indice >= 0 && indice < array.length) {
         console.log("El valor en el índice", indice, "es:", array[indice]);
     } else {
@@ -7,7 +7,6 @@ function imprimirValorEnIndice(array, indice) {
     }
 }
 
-// Ejemplo de uso:
 let array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
 let indice = 1;
-imprimirValorEnIndice(array, indice); // Output: El valor en el índice 1 es: 6
+imprimirValorEnIndice(array, indice); 
