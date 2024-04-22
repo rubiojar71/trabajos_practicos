a835f4e357d0856d79d2c654d7db3fbd60db50c9
rubiojar71 <40005819+rubiojar71@users.noreply.github.com>
2024-04-21 23:43:44 -0300

    Update punto15.js

--- punto15.js
+++ punto15.js
@@ -1,11 +1,10 @@
 function esBisiesto(anio) {
-    // Un año es bisiesto si es divisible entre 4
-    // Pero no es bisiesto si es divisible entre 100, a menos que también sea divisible entre 400
+
     return (anio % 4 === 0 && anio % 100 !== 0) || (anio % 400 === 0);
 }
 
-// Ejemplo de uso:
-let anio = 2024; // Ejemplo de año bisiesto
+
+let anio = 2024; 
 if (esBisiesto(anio)) {
     console.log(anio + " es un año bisiesto.");
 } else {
